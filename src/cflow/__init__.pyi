287ceda037486f5b78a13e2a0820206b492d243a
maromei <maromei@proton.me>
2026-05-16 19:58:41 +0200

    feat: add stub files
    - contain script to generate files
    - added mypy as dependency for stubgen script
    - added generated .pyi files

---
+++ src/cflow/__init__.pyi
@@ -0,0 +1,5 @@
+from cflow._error import CflowException as CflowException, ExpectedException as ExpectedException, UnpackingException as UnpackingException
+from cflow._option import Nothing as Nothing, Option as Option, OptionProtocol as OptionProtocol, Some as Some
+from cflow._unit import Unit as Unit
+
+__all__ = ['Some', 'Nothing', 'Option', 'OptionProtocol', 'Unit', 'CflowException', 'UnpackingException', 'ExpectedException']
